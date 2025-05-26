ae5de28684cdfbd71818c790cbc0c19d0f850724
jorgebusca <buscajorge@gmail.com>
2025-05-26 10:13:13 -0300

    crear BD

--- mipagina/myapp/form.py
+++ mipagina/myapp/form.py
@@ -1,8 +1,10 @@
 
-from django.forms import ModelForm   # Esta clase nos permite extenderla creando nuestra clase
+from django.forms import ModelForm 
+# Esta clase nos permite extenderla creando nuestra clase
 from .models import Tareas
 
 class TareasForm(ModelForm):
 	class Meta:
 		model = Tareas
-		fields = ['nombre', 'descripcion', 'precio', 'imagen']#NOMBRO LOS CAMPOS QUE QUIERO UTILIZAR 
+		fields = ['nombre', 'descripcion', 'precio', 'imagen']
+#NOMBRO LOS CAMPOS QUE QUIERO UTILIZAR 
